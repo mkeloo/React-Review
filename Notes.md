ebadedb68f7a28ed9cde7dcb2b35e88dd73fd012
mkeloo <mkeloo@ufl.edu>
2022-09-04 04:04:19 -0400

    Props Review

--- Notes.md
+++ Notes.md
@@ -163,6 +163,7 @@ const Author = () => (
 
 function BookList() {
 return (
+
 <section className="booklist">
 <Book />
 <Book />
@@ -182,6 +183,7 @@ const place = 'London, United Kingdom';
 const Book = () => {
 const title = 'Hello World Book Title';
 return (
+
 <article className="book">
 <img
 style={{ borderRadius: '5rem 2rem' }}
@@ -203,3 +205,57 @@ const Author = () => (
 );
 
 <!-- Props -->
+
+// Mini Book Project
+
+// setup vars
+const firstBook = {
+img: 'https://res.cloudinary.com/uktv/image/upload/b_rgb:000000,w_424,h_238/v1474542171/qatgchfqx4in7h58ianu.jpg',
+author: 'Jose Artillo',
+title: 'Hello World Book Title',
+place: 'London, United Kingdom',
+};
+
+const secondBook = {
+img: 'https://media.gq.com/photos/5ad64204c8be07604e8b5f2f/3:2/w_1998,h_1332,c_limit/21-books-GQ-April-2018-041718-3x2.jpg',
+author: 'J.D. Salinger',
+title: 'Catcher of the Rye',
+place: 'Jacksonville, USA',
+};
+
+function BookList() {
+return (
+
+<section className="booklist">
+<Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+        place={firstBook.place}
+      />
+<Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+        place={secondBook.place}
+      />
+</section>
+);
+}
+
+const Book = (props) => {
+return (
+
+<article className="book">
+<img style={{ borderRadius: '5rem 2rem' }} src={props.img} alt="Book" />
+<h1>{props.title}</h1>
+<h4>{props.author}</h4>
+<h5 className="place">{props.place}</h5>
+{/_ <p>{props.job}</p>
+<p>{props.title}</p>
+<p>{props.number}</p> _/}
+</article>
+);
+};
+
+<!-- Props Destructuring -->
--- src/index.js
+++ src/index.js
@@ -7,42 +7,51 @@ import './index.css';
 // Mini Book Project
 
 // setup vars
-const author = 'Jose Artillo';
-const place = 'London, United Kingdom';
-const title = 'Hello World Book Title';
-const img =
-  'https://res.cloudinary.com/uktv/image/upload/b_rgb:000000,w_424,h_238/v1474542171/qatgchfqx4in7h58ianu.jpg';
+const firstBook = {
+  img: 'https://res.cloudinary.com/uktv/image/upload/b_rgb:000000,w_424,h_238/v1474542171/qatgchfqx4in7h58ianu.jpg',
+  author: 'Jose Artillo',
+  title: 'Hello World Book Title',
+  place: 'London, United Kingdom',
+};
+
+const secondBook = {
+  img: 'https://media.gq.com/photos/5ad64204c8be07604e8b5f2f/3:2/w_1998,h_1332,c_limit/21-books-GQ-April-2018-041718-3x2.jpg',
+  author: 'J.D. Salinger',
+  title: 'Catcher of the Rye',
+  place: 'Jacksonville, USA',
+};
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+        place={firstBook.place}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+        place={secondBook.place}
+      />
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
   return (
     <article className="book">
-      <img style={{ borderRadius: '5rem 2rem' }} src={img} alt="Book" />
-      <h1>{title}</h1>
-      <Author />
-      <h5 className="place">{place.toUpperCase()}</h5>
+      <img style={{ borderRadius: '5rem 2rem' }} src={props.img} alt="Book" />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
+      <h5 className="place">{props.place}</h5>
+      {/* <p>{props.job}</p>
+      <p>{props.title}</p>
+      <p>{props.number}</p> */}
     </article>
   );
 };
-
-const Author = () => (
-  <h4 style={{ color: '#617d98', fontSize: '1.2rem', marginTop: '0.25rem' }}>
-    {author}
-  </h4>
-);
 
 ReactDom.render(<BookList />, document.getElementById('root'));
